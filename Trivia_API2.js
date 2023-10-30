9914aba59f46dce8381965f9c0ac8fa112b50f15
Tshepiso <malatjitshepiso671@gmail.com>
2023-10-30 12:34:07 +0200

    V.2

--- Trivia_API2.js
+++ Trivia_API2.js
@@ -1,52 +1,51 @@
 // @ts-nocheck
 const url2 = "https://opentdb.com/api_category.php"
-const url3 = "https://opentdb.com/api_difficulty.php";
-const opt2 = { method: "GET", headers: { Accept: "application/json" } };
+const opt2 = {method : "GET", headers:{Accept: "application/json"}}
 
-let allCategories = [];
-let categoryName;
-let categoryId;
-let selectOptions = [];
-let select = document.getElementById("category");
 
-async function getCategories() {
-  try {
-    const res2 = await fetch(url2, opt2);
-    const data2 = await res2.json();
+let allCategories = []
+let categoryName
+let categoryId 
+let selectOptions = []
+async function categories(){
+    let catId = localStorage.getItem('Id')|| 1
+    try {
+        const res = await fetch(url2,opt2)
+        const data = await res.json()
+        
+        
+        data.trivia_categories.forEach(trivia_categorie=>
+        {
+            allCategories = data.trivia_categories
+            selectOptions = data.trivia_categories.id
+        })
 
-    allCategories = data2.trivia_categories;
+        let select = document.getElementById('Category')
+        allCategories.forEach(category=>{
+            
+            let option = document.createElement('option')
+            option.setAttribute('id', category.id)
+            option.setAttribute('class', 'option-list')
+            option.innerText= category.name
+            select.appendChild(option)
+        
+        })
 
-    allCategories.forEach((category) => {
-      let option = document.createElement("option");
-      option.innerText = category.name;
-      option.setAttribute("id", category.id);
-      select.appendChild(option);
-    });
-  } catch (error) {
-    console.error("ERROR", error);
-  }
+        //document.querySelector('Category')
+        // console.log(select)
+        // selectOptions.forEach(elm =>{
+        //     console.log(elm)
+        // })
+        
+            
+    } catch (error) {
+        console.log(error)
+    }
 }
-getCategories();
+
+categories()
 
 
-
-
-
-async function getDifficulties() {
-  try {
-    const res3 = await fetch(url3, opt2);
-    const data3 = await res3.json();
-
-    allDifficulties = data3.trivia_difficulties;
-
-    console.log(allDifficulties);
-  } catch (error) {
-    console.error("ERROR", error);
-  }
-}
-// getDifficulties();
-
-    
 
 //data = requests.get("https://opentdb.com/api.php?amount=1&category={}&difficulty=easy".format(category_id)).json()
 
